f6458ca321545cdb1d457ecaa0143837b0d462af
Thiago <thiagoassis.99@gmail.com>
2022-12-10 15:42:53 -0300

    initial commit

--- CodecovDemoTests/CodecovDemoTests.swift
+++ CodecovDemoTests/CodecovDemoTests.swift
@@ -16,7 +16,7 @@ class CodecovDemoTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    }	
     
     func testNameInitialization() {
         let nameInit = Person(firstName: "Grace", lastName: "Njoroge")
